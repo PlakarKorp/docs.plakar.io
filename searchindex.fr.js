819026641d6980791a32c29a8b65f6cbb8eb9518
poolpOrg <poolpOrg@users.noreply.github.com>
2025-02-25 19:53:01 +0000

    Publishing Site PlakarKorp/docs.plakar.io at 6c22134ea576a30e66cb8c4283fffdee788ba54e on Tue Feb 25 19:53:01 UTC 2025

---
+++ searchindex.fr.js
@@ -0,0 +1,26 @@
+var relearn_searchindex = [
+  {
+    "breadcrumb": "docs.plakar.io",
+    "content": "",
+    "description": "",
+    "tags": [],
+    "title": "Catégories",
+    "uri": "/fr/categories/index.html"
+  },
+  {
+    "breadcrumb": "",
+    "content": "",
+    "description": "",
+    "tags": [],
+    "title": "docs.plakar.io",
+    "uri": "/fr/index.html"
+  },
+  {
+    "breadcrumb": "docs.plakar.io",
+    "content": "",
+    "description": "",
+    "tags": [],
+    "title": "Mots-clés",
+    "uri": "/fr/tags/index.html"
+  }
+]
